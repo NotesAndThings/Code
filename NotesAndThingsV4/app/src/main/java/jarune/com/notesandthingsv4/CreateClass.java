af89ae0fc514bf0f11f9753eee7f213cc524225d
Bryan Janes <bryanjanes@Bryans-Macintosh.local>
2016-04-15 16:35:45 -0500

    Create Class Screen
    XML file which designs the create class screen, java file that has
    spinner added, strings XML file updated with strings related to Search
    Class and Create Class screens

--- NotesAndThingsV4/app/src/main/java/jarune/com/notesandthingsv4/CreateClass.java
+++ NotesAndThingsV4/app/src/main/java/jarune/com/notesandthingsv4/CreateClass.java
@@ -6,6 +6,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class CreateClass extends AppCompatActivity {
 
@@ -15,6 +17,10 @@ public class CreateClass extends AppCompatActivity {
         setContentView(R.layout.activity_create_class);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        Spinner dropdown = (Spinner)findViewById(R.id.spinner1);
+        String[] items = new String[]{"Fall", "Spring", "Summer", "Winter"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        dropdown.setAdapter(adapter);
     }
 
 }
