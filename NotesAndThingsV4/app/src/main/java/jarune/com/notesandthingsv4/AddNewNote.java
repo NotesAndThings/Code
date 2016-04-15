cbe3ab5e0b2ae2adcdfcea514c05cb69bcd31954
Bryan Janes <bryanjanes@Bryans-Macintosh.local>
2016-04-15 14:59:24 -0500

    Search Class Screen/Removed Filler Action Button
    Designed search classes screen and removed the filler action button
    from all screens

--- NotesAndThingsV4/app/src/main/java/jarune/com/notesandthingsv4/AddNewNote.java
+++ NotesAndThingsV4/app/src/main/java/jarune/com/notesandthingsv4/AddNewNote.java
@@ -15,15 +15,8 @@ public class AddNewNote extends AppCompatActivity {
         setContentView(R.layout.activity_add_new_note);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
+
+    //does this appear as a change?
 
 }
--- NotesAndThingsV4/app/src/main/java/jarune/com/notesandthingsv4/SearchClasses.java
+++ NotesAndThingsV4/app/src/main/java/jarune/com/notesandthingsv4/SearchClasses.java
@@ -15,15 +15,6 @@ public class SearchClasses extends AppCompatActivity {
         setContentView(R.layout.activity_search_classes);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 }
