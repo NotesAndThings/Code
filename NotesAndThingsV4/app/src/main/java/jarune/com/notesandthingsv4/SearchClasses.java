18fe2cbc75a1cc1144beee63777ae1e0e23bcdc8
Bryan Janes <bryanjanes@Bryans-Macintosh.local>
2016-04-25 14:11:05 -0500

    Search Classes
    Adds functionality to search classes

--- NotesAndThingsV4/app/src/main/java/jarune/com/notesandthingsv4/SearchClasses.java
+++ NotesAndThingsV4/app/src/main/java/jarune/com/notesandthingsv4/SearchClasses.java
@@ -8,7 +8,10 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import java.io.IOException;
@@ -35,35 +38,35 @@ public class SearchClasses extends AppCompatActivity {
 
         if(searchText.getText().toString().isEmpty())
         {
-            Toast.makeText(this, "Please Enter Class To Search", Toast.LENGTH_SHORT);
+            Toast.makeText(this, "Please Enter Class To Search", Toast.LENGTH_SHORT).show();
         }
+        else {
+            sql = new searchSql();
+            sql.execute(searchText.getText().toString(), university, "");
+            //String stuff = SqlHelper.searchclasses(searchText.getText().toString(), university);
+            //parsing stuff and what not
 
-        sql = new searchSql();
-        sql.execute(searchText.getText().toString(), university, "");
-        //String stuff = SqlHelper.searchclasses(searchText.getText().toString(), university);
-        //parsing stuff and what not
+            try {
+                results = sql.get();
 
-        try {
-            results = sql.get();
-
-            String[] splitString = results.split("\\|");
-            if (splitString.length > 0) {
-                id = splitString[0];
-                ArrayList<Course> courses = new ArrayList<>();
-                if (splitString.length > 2) {
-                    for (int i = 1; i < splitString.length; i = i + 7) {
-                        Course course = new Course(splitString[i], splitString[i + 1], splitString[i + 2], splitString[i + 3], splitString[i + 4], splitString[i + 5], splitString[i + 6]);
-                        courses.add(course);
+                String[] splitString = results.split("\\|");
+                System.out.println(splitString.length);
+                if (splitString.length > 0) {
+                    ArrayList<Course> courses = new ArrayList<>();
+                    if (splitString.length > 2) {
+                        for (int i = 0; i < splitString.length; i = i + 7) {
+                            Course course = new Course(splitString[i], splitString[i + 1], splitString[i + 2], splitString[i + 3], splitString[i + 4], splitString[i + 5], splitString[i + 6]);
+                            courses.add(course);
+                        }
                     }
+                    searchSuccessful(courses);
                 }
-                searchSuccessful(courses);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
             }
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
         }
-
     }
 
     class searchSql extends AsyncTask<String, String, String> {
@@ -101,13 +104,13 @@ public class SearchClasses extends AppCompatActivity {
     }
 
     public void searchSuccessful(ArrayList<Course> courses) {
-        Intent intent = new Intent(SearchClasses.this, SearchClasses.class);
-        intent.putExtra("id", id);
-        intent.putExtra("courseCount", courses.size());
-        if(courses.size() > 0) {
-            intent.putExtra("courses", courses);
+        ListView listView = (ListView) findViewById(R.id.search_class_list);
+        ArrayList<String> courseNames = new ArrayList<>();
+        for(int i = 0; i < courses.size(); i++) {
+            courseNames.add(courses.get(i).getName() + " - " + courses.get(i).getInstructor());
         }
-        startActivity(intent);
+        ListAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, courseNames);
+        listView.setAdapter(adapter);
     }
 
 }
